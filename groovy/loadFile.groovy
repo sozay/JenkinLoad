d35614d2f0f2e362b894a6597f8e22c9e59aab77
serdar ozay <serdarozay@vakts-MacBook-Pro-2.local>
2019-04-03 17:50:43 +0100

    initials

---
+++ groovy/loadFile.groovy
@@ -0,0 +1,30 @@
+#!/usr/bin/env groovy
+
+def call(fileSize, fileCount){
+
+  stage('Create Files') {
+      script {
+        def buildImage = docker.build("v-ops-build:${env.BUILD_ID}", "-f config/Dockerfile config")
+
+        def current_dir = pwd();
+
+        buildImage.inside(
+            "-v ${HOME}/.aws:/root/.aws \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            -w ${current_dir} \
+            -u 'root' \
+            -e USER=${USER} \
+            -e AWS_DEFAULT_REGION=eu-west-1 \
+            -e ANSIBLE_CONFIG=/app/infrastructure/ansible.cfg \
+            --net=host"
+        ) {
+            sh "account_name=${accountName} environment_identifier=${environmentIdentifer} component_name=${componentName} make ${target}"
+
+        }
+      }
+
+  }
+}
+
+
+return this;
