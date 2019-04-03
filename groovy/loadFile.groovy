c7bd238c5d14fb3cdde69695ed86a78426fe4fd0
serdar ozay <sozay@users.noreply.github.com>
2019-04-03 22:28:17 +0100

    Update loadFile.groovy

--- groovy/loadFile.groovy
+++ groovy/loadFile.groovy
@@ -1,9 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(fileSize, fileCount){
-  stage('Checkout') {
-    checkout scm
-  }
+ 
   stage('Create Files') {
       script {
         writeFile file: 'groovy1.txt', text: 'Working with files the Groovy way is easy.'
